bbe7bd9d9a047d1fbb8f6122be5e474126c2a9e1
Vikas <vikasgupta3634@gmail.com>
2022-01-12 16:23:48 +0530

    last commit

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,27 @@ import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 export default function Index() {
   return (
     <>
+     <Head>
+        <title>Vikas Portfolio</title>
+        <meta
+          name="title"
+          property="og:title"
+          content="Vikas Portfolio"
+        />
+        <meta property='og:type' content='website' />
+        <meta
+          name="description"
+          property="og:description"
+          content="Vikas Portfolio using Next.js"
+        />
+        <meta name="twitter:title" content="Vikas Portfolio" />
+        <meta name="twitter:description" content="Vikas Portfolio using Next.js" />
+      </Head>
       <Home />
       <Navbar />
       <About />
