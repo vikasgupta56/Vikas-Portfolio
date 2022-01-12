b79a5ff5476991c11beee727ac4fb57c83334aee
Vikas <vikasgupta3634@gmail.com>
2022-01-12 16:28:50 +0530

    vikas

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
 import '../styles/portfolio.css';
 import '../styles/navbar.css';
+import '../styles/about.css';
+import '../styles/contact.css';
+import '../styles/footer.css';
+import '../styles/home.css';
+import '../styles/projects.css';
+import '../styles/skills.css';
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps }
